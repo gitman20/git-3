54e7d43754bff7148d87f611bc723a8fa753bd58
jaganrs <73040335+jaganrs@users.noreply.github.com>
2020-10-18 02:34:22 +0530

    Create c3.cpp

---
+++ c3.cpp
@@ -0,0 +1,10 @@
+
+   #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w", stderr);
+   #endif
+
+   #ifndef ONLINE_JUDGE
+    cerr<<(1000*clock())/CLOCKS_PER_SEC<<"ms";
+   #endif      
